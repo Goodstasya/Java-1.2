d695ae60abe8d59f97f2d916b1e990ec42dd9f57
Ilina Anastasiia <goodstasya@gmail.com>
2020-06-30 15:07:26 +0200

    Balance

--- 1.2_programming/variables/src/Main.java
+++ 1.2_programming/variables/src/Main.java
@@ -1,9 +1,10 @@
 public class Main {
   public static void main(String[] args) {
-    int price = 150;
-    int count = 5;
-    int total = price * count;
-    System.out.println(total);
+    int balance = 2_000_000_000;
+    int transfer = 500_000_000;
+    int new_balance;
+    new_balance = balance + transfer;
+    System.out.println(new_balance);
   }
 }
 
